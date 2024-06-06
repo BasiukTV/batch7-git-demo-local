41d48374606f17190559384204f813edf45b669b
TB-EIT <taras.basiuk@enhanceit.us>
2024-06-06 15:35:46 -0400

    Adding the list summation feature.

--- hello.go
+++ hello.go
@@ -4,4 +4,18 @@ import("fmt")
 
 func main() {
     fmt.Println("Hello World!")
+
+    // Define a list of integers
+    numbers := []int{10, 20, 30, 40, 50}
+
+    // Variable to store the sum
+    sum := 0
+
+    // Loop through the numbers to calculate the sum
+    for _, number := range numbers {
+        sum += number
+    }
+
+    // Print the sum
+    fmt.Println("The sum is:", sum)
 }
